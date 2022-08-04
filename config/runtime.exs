4a4d4ce24dd99abd6aa4a6effd76e47b7209780f
Chase Granberry <chase@logflare.app>
2022-08-03 19:43:01 -0700

    Another try

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,23 +29,19 @@ if config_env() == :prod do
       raise "FLY_APP_NAME not available"
 
   host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :fly_swatter, FlySwatterWeb.Endpoint,
-    server: true,
-    url: [host: "#{app_name}.fly.dev", port: 80],
+    url: [host: host, port: 443],
+    check_origin: :conn,
     http: [
-      port: String.to_integer(System.get_env("PORT") || "4000"),
-      # IMPORTANT: support IPv6 addresses
-      transport_options: [socket_opts: [:inet6]]
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
     ],
-    # http: [
-    #   # Enable IPv6 and bind on all interfaces.
-    #   # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-    #   # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-    #   # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-    #   ip: {0, 0, 0, 0, 0, 0, 0, 0},
-    #   port: String.to_integer(System.get_env("PORT") || "4000")
-    # ],
     secret_key_base: secret_key_base
 
   config :logflare_logger_backend,
